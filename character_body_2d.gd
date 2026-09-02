dc516c3355ec07a218c7d0b14eabd87ab7e5bd81
oiPoipoi <jacob.rask@waldenschool.us>
2026-09-02 14:39:06 -0600

    flip

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+@onready var icon: Sprite2D = $Icon
 @export var max_speed: float = 300.0
 @export var acceleration: float = 1500.0
 @export var friction: float = 2000.0
@@ -7,7 +7,7 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	var input_vector: Vector2 = Input.get_vector("a", "d", "w", "s")
-	
+	icon.flip_h = true if input_vector.x < 0 else false
 	if input_vector != Vector2.ZERO:
 		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
 	else:
